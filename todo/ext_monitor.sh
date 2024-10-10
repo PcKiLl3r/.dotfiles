94ee632e02597b5d5966cd3937826f9c2b6c9c7f
Grega Zorko <zorko.grega@gmail.com>
2024-10-10 18:48:21 +0200

    changes to dual monitor handling

--- todo/ext_monitor.sh
+++ todo/ext_monitor.sh
@@ -1,23 +1,9 @@
 #!/usr/bin/env sh
 
-# Get the status of the second monitor (HDMI-1)
-status=$(xrandr --query | grep "HDMI-1")
-
-# If the monitor is listed (connected or disconnected)
-if [[ -n $status ]]; then
-  # Check if the monitor is connected and currently active
-  if xrandr --query | grep "HDMI-1 connected" | grep "1920x1080"; then
-    xrandr --output HDMI-1 --off
-    echo "Second monitor turned off."
-  elif xrandr --query | grep "HDMI-1 connected"; then
-    # If the monitor is connected but off, turn it on
-    xrandr --output HDMI-1 --auto --right-of eDP-1 --scale 0.5x0.5
-    echo "Second monitor turned on."
-  else
-    # If the monitor is disconnected but has a configuration, turn it off
-    xrandr --output HDMI-1 --off
-    echo "Second monitor configuration disabled."
-  fi
-else
-  echo "HDMI-1 is not listed."
-fi
+case "$1" in
+    on) xrandr --output eDP-1 --primary --mode 1920x1200 --pos 0x0 --scale 1.5x1.5 --output HDMI-1 --mode 3840x2160 --pos 2880x0 --scale 1x1
+    echo "Second monitor turned on." ;;
+    off) xrandr --output eDP-1 --primary --mode 1920x1200 --pos 0x0 --scale 1.5x1.5 --output HDMI-1 --off
+    echo "Second monitor turned off." ;;
+    *) printf '%s\n' "You must provide an argument: DP, HDMI or DUAL" ;;
+esac
