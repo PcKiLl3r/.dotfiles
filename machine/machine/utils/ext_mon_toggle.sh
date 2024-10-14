1941927defe20353a64b5c29216a12a818b1dddd
Grega Zorko <zorko.grega@gmail.com>
2024-10-14 12:10:09 +0200

    updated monitor toggle script to correctly setup background

--- machine/machine/utils/ext_mon_toggle.sh
+++ machine/machine/utils/ext_mon_toggle.sh
@@ -4,9 +4,16 @@
 if xrandr | grep "HDMI-1 connected" > /dev/null; then
     # HDMI-1 is connected, configure it
     echo "Second monitor detected. Turning on..."
+
     xrandr --output eDP-1 --primary --mode 1920x1200 --pos 0x0 --scale 1.5x1.5 --output HDMI-1 --mode 3840x2160 --pos 2880x0 --scale 1x1
+    
+    nitrogen --set-zoom-fill ~/machine/images/backgrounds/bg.jpg --head=0
+    nitrogen --set-zoom-fill ~/machine/images/backgrounds/bg.jpg --head=1
 else
     # HDMI-1 is not connected, turn it off
     echo "No second monitor detected. Turning off..."
+
     xrandr --output eDP-1 --primary --mode 1920x1200 --pos 0x0 --scale 1.5x1.5 --output HDMI-1 --off
+
+    nitrogen --set-zoom-fill ~/machine/images/backgrounds/bg.jpg --head=0
 fi
